31950bf8290149951eccf462fe97b829d70ec585
JaxV9 <layanj.pro@gmail.com>
2024-11-07 10:29:00 +0100

    feat: send message with websocket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,43 @@
+"use client"
+
+import { useEffect } from "react";
+import { io } from "socket.io-client";
 
 export default function Home() {
+
+  // Initialisation de la connexion au serveur WebSocket
+  const socket = io('https://little-nodejs-server.onrender.com/', { autoConnect: false });
+
+  const sendMessage = (message: string) => {
+    if (!socket.connected) {
+      socket.connect();
+    }
+    socket.emit('message', { "message": message });
+  };
+
+  useEffect(() => {
+    return () => {
+      if (socket.connected) {
+        socket.disconnect();
+      }
+    };
+  }, [socket]);
+
+
   return (
     <>
       <section>
-        <div className="stock-item">
-          <span>item 1</span>
+        <div className="stock-item" onClick={() => sendMessage('Capteurs de mouvements')}>
+          <span>Capteurs de mouvements</span>
         </div>
-        <div className="stock-item">
-          <span>item 2</span>
+        <div className="stock-item" onClick={() =>sendMessage('Capteurs ultrason')}>
+          <span>Capteurs ultrason</span>
         </div>
-        <div className="stock-item">
-          <span>item 3</span>
+        <div className="stock-item" onClick={() =>sendMessage('Leds')}>
+          <span>Leds</span>
+        </div>
+        <div className="stock-item" onClick={() =>sendMessage('Résistances')}>
+          <span>Résistances</span>
         </div>
       </section>
     </>
